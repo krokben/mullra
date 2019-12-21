38084542fae64d5f1f4fd2b4192359b17b499ff1
Tommy Svensson <tommy@diesdas.digital>
2019-12-21 22:23:11 +0100

    refactor

--- src/routes.ts
+++ src/routes.ts
@@ -5,40 +5,30 @@ import { getSong, addSong, removeSong, updateSongTitle } from "./songs";
 
 const router: Router = Router();
 
-function songsHandler(req: Request, res: Response): void {
-  if (req.method === "POST") {
-    addSong(store, req.body.song);
-    res.sendStatus(202);
-    return;
-  }
-
+router.get("/songs", (req: Request, res: Response): void => {
   res.sendStatus(200);
-}
+});
 
-function songHandler(req: Request, res: Response): void {
-  if (req.method === "DELETE") {
-    removeSong(store, Number(req.params.id))
-      .then(() => res.sendStatus(200))
-      .catch(error => res.status(404).send(error));
-    return;
-  }
+router.post("/songs", (req: Request, res: Response): void => {
+  addSong(store, req.body.song);
+  res.sendStatus(202);
+});
 
-  if (req.method === "PUT") {
-    updateSongTitle(store, Number(req.params.id), req.body.title)
-      .then(() => res.sendStatus(202))
-      .catch(error => res.status(404).send(error));
-    return;
-  }
-
+router.get("/songs/:id", (req: Request, res: Response): void => {
   getSong(store, Number(req.params.id))
     .then(song => res.status(200).json(song))
     .catch(error => res.status(404).send(error));
-}
+});
 
-router.get("/songs", songsHandler);
-router.post("/songs", songsHandler);
-router.get("/songs/:id", songHandler);
-router.put("/songs/:id", songHandler);
-router.delete("/songs/:id", songHandler);
+router.put("/songs/:id", (req: Request, res: Response): void => {
+  updateSongTitle(store, Number(req.params.id), req.body.title)
+    .then(() => res.sendStatus(202))
+    .catch(error => res.status(404).send(error));
+});
+router.delete("/songs/:id", (req: Request, res: Response): void => {
+  removeSong(store, Number(req.params.id))
+    .then(() => res.sendStatus(200))
+    .catch(error => res.status(404).send(error));
+});
 
 export { router };
--- test/unit_tests/songs.test.ts
+++ test/unit_tests/songs.test.ts
@@ -1,16 +1,26 @@
 import { Store } from "../../src/server";
-import { Song, getSongs, getSong } from "../../src/songs";
+import { Song, getSongs, getSong, addSong } from "../../src/songs";
 
-test("getSongs_noSongsInDB_emptyList", async () => {
+test("get songs", async () => {
   const stubStore: Store = { songs: [] };
   const songs: Song[] = await getSongs(stubStore);
 
   expect(songs.length).toBe(0);
 });
 
-test("getSong_songInDB_song", async () => {
+test("get song", async () => {
   const exampleSong = { id: 1, title: "first song" };
   const stubStore: Store = { songs: [exampleSong] };
   const song: Song = await getSong(stubStore, 1);
   expect(song).toBe(exampleSong);
+});
+
+test("add song", async () => {
+  const stubStore: Store = { songs: [] };
+  const newSong: Song = { id: 1, title: "first song" };
+
+  await addSong(stubStore, newSong);
+
+  const song: Song = await getSong(stubStore, 1);
+  expect(song).toBe(newSong);
 });
